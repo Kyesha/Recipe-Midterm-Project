97179fd1e9f8f1870dca18114a35a08275e3f7de
Kyesha <franciskyesha1@gmail.com>
2019-07-17 09:52:42 -0400

    recipe midterm project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+import Recipe from "./component/Recipe";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+const API_ID = '2ba38d6f';
+const APP_KEY='2f41b528be9fa32eddae086148b251bd';
+const exampleReq ='https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free'
+
+const [counter , setCounter] = useState(0);
+
+useEffect( () => {
+
+
+}, []);
+
+
+const getRecipes = async () => {
+  const response = await fetch('https://api.edamam.comsearch?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}'
+);
+const data = await response.json();
+console.log(data);
+
+setRecipes(data.hits);
+};
+
+return (
+      <div className="App">
+        <form className="search-form">
+          <input className="search-bar" type="text"/>
+          <button className="search-button" type="submit">
+            search
+          </button>
+        </form>
+        {recipe.map(recipe => (
+          <Recipe />
+        ))}
+        ;
+      </div>
+);
+};
+
+
+
 
 export default App;
---
+++ src/components/Recipe.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Recipe from "./component/Recipe";
+const Recipe = () => {
+  return(
+    <div>
+    <h1>Title</h1>
+    <p>Calories</p>
+    <img src="" alt=""/>
+    </div>
+  ):
+}
+
+export default Recipe;
